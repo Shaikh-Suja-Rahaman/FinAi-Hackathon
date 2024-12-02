b91df67b5f946af6d02a6acfecc3876c4819038a
JoeDaniel <very.joe.daniel@gmail.com>
2024-12-02 11:19:55 +0530

    added the time greet function

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,6 +1,18 @@
-let backButton = document.querySelector('#backButton')
+const backButton = document.querySelector("#backButton");
+const greetings = document.querySelector(".greetings");
 
-backButton.addEventListener('click', (e) => {
-    window.location.href = "../login/login.html"
-    localStorage.removeItem('isLoggedIn')
-})
+backButton.addEventListener("click", (e) => {
+  window.location.href = "../login/login.html";
+  localStorage.removeItem("isLoggedIn");
+});
+
+const newDate = new Date();
+let time = newDate.getHours();
+
+if (time >= 0 && time < 12) {
+  greetings.innerText = "Good Morning,";
+} else if (time >= 12 && time < 17) {
+  greetings.innerText = "Good Afternoon,";
+} else if (time >= 17 && time < 24) {
+  greetings.innerText = "Good Evening,";
+}
