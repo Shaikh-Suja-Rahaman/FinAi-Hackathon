1fa6d76679d7945ce062a6a5ee5748b67d165721
JoeDaniel <very.joe.daniel@gmail.com>
2024-12-04 15:47:38 +0530

    fine tuned the model

--- Dashboard/chat.py
+++ Dashboard/chat.py
@@ -1,6 +1,9 @@
 import google.generativeai as genai
 
+
+
 genai.configure(api_key="AIzaSyCimAqa7BwLamEjdVYVLQCQHKJZRoM9p0Y")
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
+response = model.generate_content("By the next prompt, you will be recieving some data regarding money spent and the cateogary. you are supposed to return me some advice on spending how to spend more cautiously next time.you dont have to provide any response now. ready?")
+
 print(response.text)
